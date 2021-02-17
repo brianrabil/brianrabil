2aa601a3b381ebd05b6ea4edfdbd5487c2551a62
Brian Rabil <17106005+BrianRabil@users.noreply.github.com>
2021-02-17 11:56:13 -0500

    Footer styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,8 +4,8 @@ export default () => (
     <footer className={`flex flex-col bg-gradient-to-r from-purple-400 via-pink-500 to-red-500`}>
         <div className={`flex flex-col items-center py-16`}>
             <div className={`text-center text-4xl flex flex-col leading-relaxed mb-8 text-white`}>
-                <span>Lets work together.</span>
-                <span>BrianRabil@gmail.com</span>
+                <span className={`font-black`}>Lets work together.</span>
+                <span className={`text-3xl`}>BrianRabil@gmail.com</span>
             </div>
             <span className={`text-5xl`}>🚀</span>
             
