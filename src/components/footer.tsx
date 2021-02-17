631bbb52af5a05893e16863f3dae45cd89890e16
Brian Rabil <17106005+BrianRabil@users.noreply.github.com>
2021-02-17 10:51:18 -0500

    Copy, colors

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
 
 export default () => (
-    <footer className={`flex flex-col bg-blue-50`}>
-        <div className={`border-b border-blue-200 flex flex-col items-center py-16`}>
-            <div className={`text-center text-4xl flex flex-col leading-relaxed mb-8`}>
+    <footer className={`flex flex-col bg-gradient-to-r from-purple-400 via-pink-500 to-red-500`}>
+        <div className={`flex flex-col items-center py-16`}>
+            <div className={`text-center text-4xl flex flex-col leading-relaxed mb-8 text-white`}>
                 <span>Lets work together.</span>
                 <span>BrianRabil@gmail.com</span>
             </div>
             <img src={`http://placehold.it/100x50`} />
         </div>
-        <div className={`py-4 px-4 sm:px-6 lg:px-8 text-gray-500`}>
+        <div className={`text-sm py-4 px-4 sm:px-6 lg:px-8 text-purple-200`}>
             © {new Date().getFullYear()} <a href="https://www.brianrabil.com">Brian Rabil</a>
         </div>
 
--- src/components/jumbotron.tsx
+++ src/components/jumbotron.tsx
@@ -4,7 +4,7 @@ export default () => {
     return (
         <div className="py-24">
             <div className={`max-w-2xl`}>
-                <h4 className={`text-lg font-medium mb-3 text-blue-600 dark:text-blue-300`}>Hello! 👋</h4>
+                <h4 className={`text-lg font-medium mb-3 text-red-500 dark:text-red-300`}>Hello! 👋</h4>
                 <h2 className={`text-2xl text-black dark:text-white leading-tight`}>
                     <p className={`mb-4`}>
                         I'm a UI/UX designer and developer based in the Washington DC area. Ever since a young age, I've been interested in the power of good design for creating impactful and meaningful experiences for users. My style is clean, functional, intuitive, corporate design. Over the course of my career, I've designed/developed solutions for the CIA, Department of Defense, the Marines, Foxconn, SAIC, several banks and hospitals, and entrepreneurs just starting out.
