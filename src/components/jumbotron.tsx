fae9c4d5d2a62d3d61949f8c959c8a5686f87ad2
Brian Rabil <17106005+BrianRabil@users.noreply.github.com>
2021-02-17 11:50:50 -0500

    Copy adjustments

--- src/components/jumbotron.tsx
+++ src/components/jumbotron.tsx
@@ -7,7 +7,7 @@ export default () => {
                 <h4 className={`text-lg font-medium mb-3 flex items-center`}><span className={`bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-red-400 block`}>Hello!</span> <span>👋</span></h4>
                 <h2 className={`text-2xl text-black dark:text-white leading-tight`}>
                     <p className={`mb-4`}>
-                        I'm a UI/UX designer and developer based in the Washington DC area. Ever since a young age, I've been interested in the power of good design for creating impactful and meaningful experiences for users. My style is clean, functional, intuitive, corporate design. Over the course of my career, I've designed/developed solutions for the CIA, Department of Defense, the Marines, Foxconn, SAIC, several banks and hospitals, and entrepreneurs just starting out.
+                        I'm a UI/UX designer and developer based in the Washington DC area. Ever since a young age, I've been interested in the power of good design and its ability to elevate the user's experience. My style is clean, functional, intuitive, corporate design. Over the course of my career, I've designed/developed solutions for the CIA, Department of Defense, the Marines, Foxconn, SAIC, several banks and hospitals, and entrepreneurs just starting out.
                     </p>
                 </h2>
             </div>
